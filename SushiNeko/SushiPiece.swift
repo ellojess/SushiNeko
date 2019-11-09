e1b21d3adbed1a33f5b31181d010e875f09a5e01
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-11-08 16:24:16 -0800

    create sushi

---
+++ SushiNeko/SushiPiece.swift
@@ -0,0 +1,54 @@
+//
+//  SushiPiece.swift
+//  SushiNeko
+//
+//  Created by Bo on 11/8/19.
+//  Copyright © 2019 Jessica Trinh. All rights reserved.
+//
+
+import SpriteKit
+
+class SushiPiece: SKSpriteNode {
+
+    /* Chopsticks objects */
+    var rightChopstick: SKSpriteNode!
+    var leftChopstick: SKSpriteNode!
+    
+    /* Sushi type */
+    var side: Side = .none {
+        didSet {
+            switch side {
+            case .left:
+                /* Show left chopstick */
+                leftChopstick.isHidden = false
+            case .right:
+                /* Show right chopstick */
+                rightChopstick.isHidden = false
+            case .none:
+                /* Hide all chopsticks */
+                leftChopstick.isHidden = true
+                rightChopstick.isHidden = true
+            }
+        }
+    }
+
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func connectChopsticks() {
+        /* Connect our child chopstick nodes */
+        rightChopstick = childNode(withName: "rightChopstick") as! SKSpriteNode
+        leftChopstick = childNode(withName: "leftChopstick") as! SKSpriteNode
+        
+        /* Set the default side */
+        side = .none
+    }
+
+}
